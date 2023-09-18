461e823e2d862cc7c7c5835832c38bb8fcda5336
JeremyPerezSwift <jeremyperez34000@gmail.com>
2023-09-18 08:08:32 +0200

    Creating the registration screen with storyboard

--- TiktokClone/ViewController.swift
+++ TiktokClone/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
+    func setupView() {
+        signupButton.layer.cornerRadius = 18
+        facebookButton.layer.cornerRadius = 18
+        googleButton.layer.cornerRadius = 18
+        loginButton.layer.cornerRadius = 18
+    }
 
 }
 
